4730226b155c792970e351f3b81b8659c43db651
charlottelevick <lottielevick_@hotmail.co.uk>
2024-11-23 14:03:46 +0000

    Added updates from slack
    Fixed analysis section
    Moved name prompting outside of function
    Created global total guesses list

--- guessing_game.py
+++ guessing_game.py
@@ -12,21 +12,23 @@ from statistics import mode
 
 # Create the start_game function.
 # Write your code inside this function.
+total_guesses= []
+
+name = input("Hello and welcome to 'The Number Guessing Game! What's your name? ")
+user_choice = input("Hi {}! Would you like to play?\n(Please enter y/n) ".format(name))
+if user_choice.lower() == "y":
+    print("Great! Please choose a number between 1 and 100...")
+else:
+    print("Ok no worries, see you later {}!".format(name))
+
 def start_game():
     #   When the program starts, we want to:
     #   ------------------------------------
     #   1. Display an intro/welcome message to the player.
-    name = input("Hello and welcome to 'The Number Guessing Game! What's your name? ")
-    user_choice = input("Hi {}! Would you like to play?\n(Please enter y/n) ".format(name))
-    if user_choice.lower() == "y":
-        print("Great! Please choose a number between 1 and 100...")
-    else:
-        print("Ok no worries, see you later {}!".format(name))
-
     #   2. Store a random number as the answer/solution.
     number = (random.randint(1,100))
     guess = int(input("Enter your guess: "))
-    list_of_guesses = [guess + 1]
+    list_of_guesses = [guess]
     #   3. Continuously prompt the player for a guess.
     #     a. If the guess is greater than the solution, display to the player "It's lower".
     #     b. If the guess is less than the solution, display to the player "It's higher".
@@ -40,25 +42,27 @@ def start_game():
             guess = int(input("Enter your guess: "))
             list_of_guesses.append(guess)
         elif (guess == number):
+            list_of_guesses.append(guess)
             break
 
     #   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list.
     print("Congratulations {}! You got it!".format(name))
     #   5. Display the following data to the player
     #     a. How many attempts it took them to get the correct number in this game
-    length_of_guesses_list = len(list_of_guesses)
-    print("It took you {} time(s) to guess the number {}".format(length_of_guesses_list, number))
+    guesses_to_win = len(list_of_guesses)
+    total_guesses.append(guesses_to_win)
+    print("It took you {} time(s) to guess the number {}".format(guesses_to_win, number))
 
     #     b. The mean of the saved attempts list
     print("Overall Statistics:")
-    saved_attempts_mean = mean(list_of_guesses)
+    saved_attempts_mean = mean(total_guesses)
     print("Mean: {}".format(saved_attempts_mean))
     #     c. The median of the saved attempts list
-    saved_attempts_median = median(list_of_guesses)
+    saved_attempts_median = median(total_guesses)
     print("Median: {}".format( saved_attempts_median))
     # d. The mode of the saved attempts list
-    saved_attempts_mode = mode(list_of_guesses)
-    print("Mode: ".format(saved_attempts_mode))
+    saved_attempts_mode = mode(total_guesses)
+    print("Mode: {}".format(saved_attempts_mode))
 
 #   6. Prompt the player to play again
     restart = input("Want to play again?")
