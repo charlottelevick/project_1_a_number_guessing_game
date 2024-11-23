a623d4dc04f79103868a44024a7ba7031b6efcd4
charlottelevick <lottielevick_@hotmail.co.uk>
2024-11-23 17:30:08 +0000

    Added error handling
    Tidied up main game function
    Added Min and Max range errors

--- guessing_game.py
+++ guessing_game.py
@@ -1,16 +1,12 @@
-"""
-Data Analysis Techdegree
-Project 1 - A Number Guessing Game
---------------------------------
-"""
-
 import random
 import sys
 from statistics import mean
 from statistics import median
 from statistics import mode
 
-total_guesses= []
+total_guesses = []
+MAX_RANGE = 100
+MIN_RANGE = 1
 
 name = input("Hello and welcome to 'The Number Guessing Game! What's your name? ")
 user_choice = input("Hi {}! Would you like to play?\n(Please enter y/n) ".format(name))
@@ -19,24 +15,36 @@ if user_choice.lower() == "y":
 else:
     print("Ok no worries, see you later {}!".format(name))
     sys.exit()
-
-def start_game():
-    number = (random.randint(1,100))
-    guess = int(input("Enter your guess: "))
-    list_of_guesses = [guess]
     
-    while guess != number:
+def start_game():
+    number = (random.randint(MIN_RANGE,MAX_RANGE))
+    list_of_guesses = []
+    is_playing = True
+
+    while is_playing == True:
+        guess = input("Enter your guess: ")
+        
+        try:
+            guess = int(guess)
+        except ValueError as err:
+            print("There was an error, please choose a number")
+            continue
+        
+        if (guess > MAX_RANGE or guess < MIN_RANGE):
+            print("Please choose a number bewteen {} and {}...".format(MIN_RANGE, MAX_RANGE))
+            continue
+        
         if (guess > number):
             print("It's lower. Try again: ")
-            guess = int(input("Enter your guess: "))
             list_of_guesses.append(guess)
-        elif (guess < number):
+        
+        if (guess < number):
             print("It's higher. Try again: ")
-            guess = int(input("Enter your guess: "))
             list_of_guesses.append(guess)
-        elif (guess == number):
+        
+        if (guess == number):
             list_of_guesses.append(guess)
-            break
+            is_playing = False
 
     print("Congratulations {}! You got it!".format(name))
    
