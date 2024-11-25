6414cb3e3cacfb64a657541319759f472764d914
charlottelevick <lottielevick_@hotmail.co.uk>
2024-11-25 18:18:40 +0000

    Adding comments

--- guessing_game.py
+++ guessing_game.py
@@ -1,13 +1,16 @@
+#Import random, sys and mean, median and mode from statistics
 import random
 import sys
 from statistics import mean
 from statistics import median
 from statistics import mode
 
+#Constants
 total_guesses = []
 MAX_RANGE = 100
 MIN_RANGE = 1
 
+#Welcome/Goodbye message and start_game function
 name = input("Hello and welcome to 'The Number Guessing Game! What's your name? ")
 user_choice = input(f"Hi {name}! Would you like to play?\n(Please enter y/n) ")
 if user_choice.lower() == "y":
@@ -15,7 +18,8 @@ if user_choice.lower() == "y":
 else:
     print(f"Ok no worries, see you later {name}!")
     sys.exit()
-    
+
+#Game function
 def start_game():
     number = (random.randint(MIN_RANGE,MAX_RANGE))
     list_of_guesses = []
@@ -23,13 +27,13 @@ def start_game():
 
     while is_playing == True:
         guess = input("Enter your guess: ")
-        
+#ValueError
         try:
             guess = int(guess)
         except ValueError as err:
             print("There was an error, please choose a number")
             continue
-        
+#Guessing prompts
         if (guess > MAX_RANGE or guess < MIN_RANGE):
             print(f"Please choose a number between {MIN_RANGE} and {MAX_RANGE}...")
             continue
@@ -45,13 +49,14 @@ def start_game():
         if (guess == number):
             list_of_guesses.append(guess)
             is_playing = False
-
+#Player has won the game
     print(f"Congratulations {name}! You got it!")
    
     guesses_to_win = len(list_of_guesses)
     total_guesses.append(guesses_to_win)
     print(f"It took you {guesses_to_win} time(s) to guess the number {number}")
 
+#Overall Statistics 
     print("Overall Statistics:")
     saved_attempts_mean = mean(total_guesses)
     print(f"Mean: {saved_attempts_mean}")
@@ -61,12 +66,12 @@ def start_game():
     
     saved_attempts_mode = mode(total_guesses)
     print(f"Mode: {saved_attempts_mode}")
-
+#Highscore and restart
     restart = input(f"**Highscore: {saved_attempts_mode}**\nWant to play again?\n(Please enter y/n) ")
     if restart.lower() == "y":
         start_game()
     else:
         print(f"Ok no worries, see you later {name}!")
         sys.exit()
-
+        
 start_game()
